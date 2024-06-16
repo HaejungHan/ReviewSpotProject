d6bf6e3d4f19ac4913f91210425942e878e3f6eb
haejungHan <tinley705@gmail.com>
2024-06-16 21:14:35 +0900

    refactor: commentService/repository 리팩토링

--- src/main/java/com/sparta/reviewspotproject/repository/CommentRepository.java
+++ src/main/java/com/sparta/reviewspotproject/repository/CommentRepository.java
@@ -1,8 +1,10 @@
 package com.sparta.reviewspotproject.repository;
 
 import com.sparta.reviewspotproject.entity.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+  List<Comment> findByPostId(Long postId);
 }
 
